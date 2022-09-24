c57cba9e44e1ed1b75513f813aac9c9d77458afb
Menyhárt László <senkialfonz11@gmail.com>
2022-09-24 16:59:14 +0200

    react learning

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { HomePage, PicturesPage, VideoPage, AccountPage } from './componens';
 
 function App() {
   return (
-    <React.Fragment>
-      <h1>Image Viewer</h1>
-    </React.Fragment>
+
+    <div>
+      <BrowserRouter>
+
+        <div className='bg-primary'>
+          <div className='row'>
+            <div className='nav-item col-md-1 text-center'>
+              <Link to="/"><button className='btn btn-primary m-1'><b>Home</b></button></Link>
+              <hr className='d-md-none my-0' />
+            </div>
+            <div className='nav-item col-md-1 text-center'>
+              <Link to="/pictures"><button className='btn btn-primary m-1'><b>Képek</b></button></Link>
+              <hr className='d-md-none my-0' />
+            </div>
+            <div className='nav-item col-md-1 text-center'>
+              <Link to="/videos"><button className='btn btn-primary m-1'><b>Videok</b></button></Link>
+              <hr className='d-md-none my-0' />
+            </div>
+            <div className='nav-item col-md-1 text-center'>
+              <Link to="/login"><button className='btn btn-primary m-1'><b>Belépés</b></button></Link>
+              <hr className='d-md-none my-0' />
+            </div>
+            <div className='nav-text col d-none d-xl-block text-center'>
+              <h4 className="navbar-text">Nézegessen kedvére szórakoztató tartalmakat!</h4>
+            </div>
+          </div>
+        </div>
+
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="/pictures" element={<PicturesPage />} />
+          <Route path="/videos" element={<VideoPage />} />
+          <Route path="/login" element={<AccountPage />} />
+        </Routes>
+
+      </BrowserRouter>
+    </div >
+
   );
 }
 
---
+++ src/componens.js
@@ -0,0 +1,23 @@
+export const HomePage = () => {
+    return (
+        <h1>Ez a Home oldal</h1>
+    )
+};
+
+export const PicturesPage = () => {
+    return (
+        <h1>Ez a képek oldal</h1>
+    )
+}
+
+export const VideoPage = () => {
+    return (
+        <h1>Ez a video oldal</h1>
+    )
+}
+
+export const AccountPage = () => {
+    return (
+        <h1>Ez az account oldal</h1>
+    )
+}
