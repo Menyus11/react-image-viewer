896225994eb44d3f3df98cf75fd12a50a902a761
Menyhárt László <senkialfonz11@gmail.com>
2022-09-25 19:27:04 +0200

    react learning

--- src/componens.js
+++ src/componens.js
@@ -1,36 +1,137 @@
-import { useState } from "react";
+
+import React, { useState, useCallback } from "react";
+import Gallery from "react-photo-gallery";
+import Carousel, { Modal, ModalGateway } from "react-images";
+import { photos } from "./photos";
 
 export const HomePage = () => {
+
+    /* 
+        useEffect(() => {
+            pictureHandler();
+            videoHandler();
+        }, []); */
+
+    const VideoHandler = () => {
+        return (
+            <h4>A videoHandler működik!</h4>
+        )
+    }
+
+    const PictureHandler = () => {
+
+        let photoIndex = "";
+
+        photos.forEach((e, i) => {
+            photoIndex = i;
+        })
+
+        const min = 0;
+        const max = photoIndex;
+        const randomPhotoIndex = Math.round(min + Math.random() * (max - min));
+        let randomPhoto = "";
+
+        photos.forEach((e, i) => {
+            if (i === randomPhotoIndex) {randomPhoto = e.src }
+        })
+
+        return (
+            <div className="m-3">
+                <img src={randomPhoto} alt={randomPhotoIndex} style={{ height: '300px', }} />
+            </div>
+        )
+    }
+
     return (
-        <h1>Ez a Home oldal</h1>
+        <div className="bg-warning">
+            <h1 className="text-center">Kezdőoldal</h1>
+            <h3 className="px-3">Videók:</h3>
+            <div className="row">
+                <VideoHandler />
+            </div>
+            <h3 className="px-3">Képek:</h3>
+            <div className="row text-center">
+                <div className='col'><PictureHandler /></div>
+                <div className='col'><PictureHandler /></div>
+                <div className='col'><PictureHandler /></div>
+                <div className='col'><PictureHandler /></div>
+            </div>
+        </div>
     )
 };
 
 export const PicturesPage = () => {
+
+    const [currentImage, setCurrentImage] = useState(0);
+    const [viewerIsOpen, setViewerIsOpen] = useState(false);
+
+    const openLightbox = useCallback((event, { photo, index }) => {
+        setCurrentImage(index);
+        setViewerIsOpen(true);
+    }, []);
+
+    const closeLightbox = () => {
+        setCurrentImage(0);
+        setViewerIsOpen(false);
+    };
+
     return (
-        <h1>Ez a képek oldal</h1>
+        <div className="bg-warning">
+            <h1 className="text-center">Kedvenc fotóim</h1>
+            <Gallery photos={photos} onClick={openLightbox} />
+            <ModalGateway>
+                {viewerIsOpen ? (
+                    <Modal onClose={closeLightbox}>
+                        <Carousel
+                            currentIndex={currentImage}
+                            views={photos.map(x => ({
+                                ...x,
+                                srcset: x.srcSet,
+                                caption: x.title
+                            }))}
+                        />
+                    </Modal>
+                ) : null}
+            </ModalGateway>
+        </div>
     )
 }
 
 export const VideoPage = () => {
     return (
-        <h1>Ez a video oldal</h1>
+        <div className="bg-warning text-center">
+            <h1 className="text-center">Kedvenc videóim</h1>
+
+            <div className="row d-flex justify-content-around">
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/MdnE-7IpaD8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/KATEnhTYGfE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/3wh7nHSp8Ic" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/uT4cEV1ijlU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/VJcEDuOAWVM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/UI5qaoZQ5G4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/LLSqc-p4UhI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/aVAOYY39wxY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+                <div className="col m-3"><iframe width="450" height="250" src="https://www.youtube.com/embed/Ul3y1LXxzdU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+            </div>
+
+        </div>
+
     )
 }
 
 export const AccountPage = () => {
 
-    const [loginData, setLoginData] = useState({loginName: "", loginPassword: ""});
-    const [regData, setRegData] = useState({regName: "", regPassword: ""});
+    const [loginData, setLoginData] = useState({ loginName: "", loginPassword: "" });
+    const [regData, setRegData] = useState({ regName: "", regPassword: "" });
 
     return (
 
-        <div className="row m-3">
+        <div className="row m-3 bg-warning">
 
             <h1 className="text-center">Lépjen be, vagy regisztráljon!</h1>
 
             <div className="text-center col m-5">
-                <div className="card" style={{ minWidth: "248px"}}>
+                <div className="card" style={{ minWidth: "248px" }}>
                     <div className="card-header">
                         Belépés
                     </div>
@@ -43,15 +144,15 @@ export const AccountPage = () => {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json'
-                                }, 
-                                body: JSON.stringify( loginData )
+                                },
+                                body: JSON.stringify(loginData)
                             })
                         }}>
                             <label htmlFor="login_email">Az Ön email címe</label><br />
-                            <input id='login_email' className="form-control" onChange={(event)=>setLoginData({...loginData, loginName: event.target.value})}/><br />
+                            <input id='login_email' className="form-control" onChange={(event) => setLoginData({ ...loginData, loginName: event.target.value })} /><br />
 
                             <label htmlFor="login_password">Az Ön jelszava</label><br />
-                            <input id='login_password' className="form-control"  onChange={(event)=>setLoginData({...loginData, loginPassword: event.target.value})}/><br />
+                            <input id='login_password' className="form-control" onChange={(event) => setLoginData({ ...loginData, loginPassword: event.target.value })} /><br />
 
                             <button className="btn btn-success m-1" type="submit">Belépés</button>
                             <button className="btn btn-danger m-1" type="button">Mégsem</button>
@@ -62,7 +163,7 @@ export const AccountPage = () => {
             </div>
 
             <div className="text-center col m-5">
-                <div className="card" style={{ minWidth: "248px"}}>
+                <div className="card" style={{ minWidth: "248px" }}>
                     <div className="card-header">
                         Regisztráció
                     </div>
@@ -75,19 +176,19 @@ export const AccountPage = () => {
                                 method: 'POST',
                                 headers: {
                                     'Content-Type': 'application/json'
-                                }, 
-                                body: JSON.stringify( regData )
+                                },
+                                body: JSON.stringify(regData)
                             })
                         }}>
                             <label htmlFor="registration_email">Az Ön email címe</label><br />
-                            <input id='registration_email' className="form-control" onChange={(event)=>setRegData({...regData, regName: event.target.value})}/><br />
+                            <input id='registration_email' className="form-control" onChange={(event) => setRegData({ ...regData, regName: event.target.value })} /><br />
 
                             <label htmlFor="registration_password">Az Ön jelszava</label><br />
-                            <input id='registration_password' className="form-control" onChange={(event)=>setRegData({...regData, regPassword: event.target.value})}/><br />
+                            <input id='registration_password' className="form-control" onChange={(event) => setRegData({ ...regData, regPassword: event.target.value })} /><br />
 
                             <button className="btn btn-success m-1" type="submit">Regisztráció</button>
                         </form>
-                        
+
                     </div>
                 </div>
             </div>
---
+++ src/photos.js
@@ -0,0 +1,122 @@
+export const photos = [
+    {
+        src: 'https://images.unsplash.com/photo-1664101055972-63f92747bb4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1568772585432-0bcd9add44f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1664058986963-5f531744c710?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1446&q=80',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1664015521235-13193b0fb560?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1664048984526-a816228740df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663959185097-e03a8c0826d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663983764047-2b1d5f599714?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663875928932-3bf9dba77e26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663937566930-22c203501fad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1584345604325-f5091269a0d1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 4.5,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1549837474-d1165d604eee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663942465109-0f79e3740be9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663860307386-891a5beb6e9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1614788689529-c5ef908d2798?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1515777315835-281b94c9589f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1512&q=80',
+        width: 4.5,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1484406566174-9da000fda645?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=778&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1527266258038-6ae3e089a609?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80',
+        width: 4,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1614602638662-c7c1f55c33f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80',
+        width: 4.5,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1436491865332-7a61a109cc05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1474&q=80',
+        width: 4.5,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1509315811345-672d83ef2fbc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+        width: 3,
+        height: 4
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1425082661705-1834bfd09dca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80',
+        width: 4.5,
+        height: 3
+    },
+    {
+        src: 'https://images.unsplash.com/photo-1663877254454-5e669a7b27e6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80',
+        width: 3.5,
+        height: 4
+    }
+];
