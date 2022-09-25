0baf8f62c9e13e2675412ecb2c94cdce0ca11220
Menyhárt László <senkialfonz11@gmail.com>
2022-09-25 07:56:48 +0200

    React learning

--- src/App.js
+++ src/App.js
@@ -10,20 +10,20 @@ function App() {
 
         <div className='bg-primary'>
           <div className='row'>
-            <div className='nav-item col-md-1 text-center'>
+            <div className='col-md-1 text-center'>
               <Link to="/"><button className='btn btn-primary m-1'><b>Home</b></button></Link>
               <hr className='d-md-none my-0' />
             </div>
-            <div className='nav-item col-md-1 text-center'>
+            <div className='col-md-1 text-center'>
               <Link to="/pictures"><button className='btn btn-primary m-1'><b>Képek</b></button></Link>
               <hr className='d-md-none my-0' />
             </div>
-            <div className='nav-item col-md-1 text-center'>
+            <div className='col-md-1 text-center'>
               <Link to="/videos"><button className='btn btn-primary m-1'><b>Videok</b></button></Link>
               <hr className='d-md-none my-0' />
             </div>
-            <div className='nav-item col-md-1 text-center'>
-              <Link to="/login"><button className='btn btn-primary m-1'><b>Belépés</b></button></Link>
+            <div className='col-md-1 text-center'>
+              <Link to="/login"><button className='btn btn-primary m-1'><b>Belépés/Regisztráció</b></button></Link>
               <hr className='d-md-none my-0' />
             </div>
             <div className='nav-text col d-none d-xl-block text-center'>
--- src/componens.js
+++ src/componens.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const HomePage = () => {
     return (
         <h1>Ez a Home oldal</h1>
@@ -17,7 +19,79 @@ export const VideoPage = () => {
 }
 
 export const AccountPage = () => {
+
+    const [loginData, setLoginData] = useState({loginName: "", loginPassword: ""});
+    const [regData, setRegData] = useState({regName: "", regPassword: ""});
+
     return (
-        <h1>Ez az account oldal</h1>
+
+        <div className="row m-3">
+
+            <h1 className="text-center">Lépjen be, vagy regisztráljon!</h1>
+
+            <div className="text-center col m-5">
+                <div className="card" style={{ minWidth: "248px"}}>
+                    <div className="card-header">
+                        Belépés
+                    </div>
+                    <div className="card-body">
+
+                        <h3 className="card-title">Itt tud belépni!</h3>
+                        <form onSubmit={(e) => {
+                            e.preventDefault();
+                            fetch('http://localhost/feldolgozo.php', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }, 
+                                body: JSON.stringify( loginData )
+                            })
+                        }}>
+                            <label htmlFor="login_email">Az Ön email címe</label><br />
+                            <input id='login_email' className="form-control" onChange={(event)=>setLoginData({...loginData, loginName: event.target.value})}/><br />
+
+                            <label htmlFor="login_password">Az Ön jelszava</label><br />
+                            <input id='login_password' className="form-control"  onChange={(event)=>setLoginData({...loginData, loginPassword: event.target.value})}/><br />
+
+                            <button className="btn btn-success m-1" type="submit">Belépés</button>
+                            <button className="btn btn-danger m-1" type="button">Mégsem</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className="text-center col m-5">
+                <div className="card" style={{ minWidth: "248px"}}>
+                    <div className="card-header">
+                        Regisztráció
+                    </div>
+                    <div className="card-body">
+
+                        <h3 className="card-title">Itt tud regisztrálni!</h3>
+                        <form onSubmit={(e) => {
+                            e.preventDefault();
+                            fetch('http://localhost/feldolgozo.php', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }, 
+                                body: JSON.stringify( regData )
+                            })
+                        }}>
+                            <label htmlFor="registration_email">Az Ön email címe</label><br />
+                            <input id='registration_email' className="form-control" onChange={(event)=>setRegData({...regData, regName: event.target.value})}/><br />
+
+                            <label htmlFor="registration_password">Az Ön jelszava</label><br />
+                            <input id='registration_password' className="form-control" onChange={(event)=>setRegData({...regData, regPassword: event.target.value})}/><br />
+
+                            <button className="btn btn-success m-1" type="submit">Regisztráció</button>
+                        </form>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
     )
 }
